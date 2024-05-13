c0ca2f2a876d7913ea87fee16ba18721a60f1a3f
damianoazzolini <damiazz94@gmail.com>
2024-05-13 13:35:20 +0200

    removed unused files

--- results/2023ilp/subsum_triple_noise/run_all.sh
+++
@@ -1,7 +0,0 @@
-sbatch run_subset_sum_triple_noise_ua_1.sh
-sbatch run_subset_sum_triple_noise_ua_2.sh
-sbatch run_subset_sum_triple_noise_ua_3.sh
-sbatch run_subset_sum_triple_noise_ua_4.sh
-sbatch run_subset_sum_triple_noise_ua_5.sh
-sbatch run_subset_sum_triple_noise_ua_6.sh
-sbatch run_subset_sum_triple_noise_ua_7.sh
